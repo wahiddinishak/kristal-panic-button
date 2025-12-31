a084f8ff72bfedb583dfe89ea34def4a9cc9d005
Wahiddin Ishak <39898870+wahiddinishak@users.noreply.github.com>
2025-12-31 10:05:52 +0700

    Adding serviceWorker

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const kristalCache = 'panic-v1';
+
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(kristalCache).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
